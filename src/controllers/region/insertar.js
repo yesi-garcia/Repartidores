52f0e93bffb4a064923f695f4ad341289d33a667
Yesenia <yesenialinares24@gmail.com>
2022-12-07 23:21:01 -0300

    se genero el archivo intertar.js en la carpeta region de controllers

---
+++ src/controllers/region/insertar.js
@@ -0,0 +1,30 @@
+const region = require('../../models/region');
+const { handleHttpError } = require('../../helpers/handleError');
+const traerPais = require('../../helpers/handleTraerPais');
+
+const insertarRegion = async(req, res, _next) {
+    const {
+        body: {
+            nombreRegion,
+            nombrePais
+        }
+    } = req;
+    try {
+        const resultPais = await traerPais(nombrePais);
+        let idPais;
+        if (resultPais.id) {
+            idPais = resultPais.id;
+        }
+        await region.create({ nombreRegion, id_pais: idPais });
+        res.status(201).json({
+            status: 'OK',
+            description: 'Creado',
+            Response: {
+                message: 'Accion realizada con satisfaccion'
+            }
+        })
+    } catch (error) {
+        handleHttpError(res, 'ERROR-EN-INGRESAR-REGISTRO', 400);
+    }
+}
+module.exports = insertarRegion;
