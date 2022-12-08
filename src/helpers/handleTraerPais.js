7957d1f2f4ed7fbffeb86da01edced35e48836a9
Yesenia <yesenialinares24@gmail.com>
2022-12-07 23:02:29 -0300

    se creo y codifico el archivo handleTraerPais para se usado en otros archivos

---
+++ src/helpers/handleTraerPais.js
@@ -0,0 +1,16 @@
+const pais = require('../models/pais');
+const { handleHttpError } = require('../helpers/handleError');
+
+const traerPais = async(nombrePais) => {
+    try {
+        const result = await pais.findOne({
+            where: {
+                nombrePais
+            }
+        })
+        return result
+    } catch (error) {
+        handleHttpError(res, 'PAIS-NO-ENCONTRADO', 404)
+    }
+}
+module.exports = traerPais;
