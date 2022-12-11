450f27daf6b5ffcdecde54197ea05606e6800f3a
Yesenia <yesenialinares24@gmail.com>
2022-12-11 15:08:33 -0300

    se creo el archivo region.js en la carpeta validators

---
+++ src/validators/region.js
@@ -0,0 +1,14 @@
+const { check } = require('express-validator');
+const validateResul = require('../helpers/handlevalidator');
+
+const validarRegistro = [
+    check('nombreRegion')
+    .exists()
+    .notEmpty()
+    .isLength({ min: 2, max: 30 })
+    .isAlpha(),
+    (req, res, next) => {
+        return validateResul(req, res, next)
+    }
+]
+module.exports = { validarRegistro };
