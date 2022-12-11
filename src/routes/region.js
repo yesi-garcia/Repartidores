3ee47d6b83415014a049e567860e15487366428a
Yesenia <yesenialinares24@gmail.com>
2022-12-11 15:14:13 -0300

    se modifico el archivo region.js de routes, indicando las validaciones

--- src/routes/region.js
+++ src/routes/region.js
@@ -1,6 +1,7 @@
 const insertarRegion = require('../controllers/region/insertar');
+const { validarRegistro } = require('../validators/region');
 const express = require('express');
 const router = express.Router();
 
-router.post('/insertar', insertarRegion);
+router.post('/insertar', validarRegistro, insertarRegion);
 module.exports = router;
